260a0c43f3fa41060fbad34f52860c26d2498caf
baiyang <17801003317@163.com>
2025-11-29 21:17:54 +0800

    glm库的使用

--- OpenGLES_Project/app/src/main/cpp/GLPrimitives.h
+++ OpenGLES_Project/app/src/main/cpp/GLPrimitives.h
@@ -8,6 +8,14 @@
 #include <GLES/gl.h>
 #include <GLES2/gl2.h>
 
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -28,7 +36,11 @@ struct  PriFloat3{
     float z;
 };
 
+float angle = 0.0f;
 void drawTriangle() {
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
     PriFloat7 vertexTriangle[] ={
             {0.5,0.1,-0.1,1.0,0.0,0.0,1.0},
             {0.5,0.5,-0.1,0.0,1.0,0.0,1.0},
@@ -40,6 +52,16 @@ void drawTriangle() {
 
     glEnableClientState(GL_COLOR_ARRAY);
     glColorPointer(4, GL_FLOAT, sizeof(PriFloat7),&vertexTriangle[0].r);
+
+    angle += 0.01f; //旋转速度
+
+    glm::mat4x4  cubeMat;
+    glm::mat4x4  cubeTransMat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -0.5));   //平移
+    glm::mat4x4  cubeRotMat   = glm::rotate(glm::mat4(1.0f),angle,glm::vec3(0.5f, 0.5f, 1.0) ); //旋转
+    glm::mat4x4  cubeScaleMat = glm::scale(glm::mat4(1.0f),glm::vec3(0.5f, 0.4f, 0.1) );        //缩放
+    cubeMat = cubeTransMat * cubeRotMat * cubeScaleMat;
+
+    glLoadMatrixf(glm::value_ptr(cubeMat));
 
     glDrawArrays(GL_TRIANGLES,0,3);
 
